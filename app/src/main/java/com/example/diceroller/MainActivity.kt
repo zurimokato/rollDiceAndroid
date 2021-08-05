04619bec710c85f3e967bc130830c7fe7e868170
zurimokato <noj2304@gmail.com>
2021-08-04 22:33:01 -0500

    adding images and some optimization

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton:Button=findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.text="Lest Roll"
 
 
@@ -23,8 +27,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val textResult:TextView=findViewById(R.id.result_text)
-        val randomInt= Random.nextInt(6)+1
-        textResult.text=randomInt.toString()
+
+        val randomInt=Random.nextInt(6)+1
+        val drawableResource=when(randomInt){
+            1 ->R.drawable.dice_1
+            2 ->R.drawable.dice_2
+            3 ->R.drawable.dice_3
+            4 ->R.drawable.dice_4
+            5 ->R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+        Log.d("random", randomInt.toString())
+        diceImage.setImageResource(drawableResource)
     }
 }
